19d6b0a6f3c77d01f183b297ca6c2a094c220f6d
Jake Murray <j1993murray@hotmail.com>
2023-11-15 19:29:38 +0000

    Add some commented tedxt in Config

--- config.lua
+++ config.lua
@@ -11,23 +11,23 @@ Config.DealerPed = {
 
 Config.ForageLocations = {
     {
-        AreaName = 'Big Shroom Tings',
-        AreaCoords = vector3(1601.67, 3222.11, 40.4),
-        AreaRadius = 15,
-        AreaCost = 1500,
+        AreaName = 'Big Shroom Tings', -- Used for context menu
+        AreaCoords = vector3(1601.67, 3222.11, 40.4), -- Where mushroom area is
+        AreaRadius = 15, -- Radius mushrooms will spawn randomly around [AreaCoords] above
+        AreaCost = 1500, -- How much it charges the player to be given each location
         MushroomsInfo = {
-            MushroomAmount = 5,
-            MushroomModel = "prop_stoneshroom1",
-            MushroomItem = "schwingleborp_mushrooms",
-            AmountPerPickup = 1,
+            MushroomAmount = 5, -- How many muhsrooms spawn
+            MushroomModel = "prop_stoneshroom1", -- Model that spawns (Can be a filing cabinet if you really fucking wanted XD)
+            MushroomItem = "schwingleborp_mushrooms", -- Name of item given to player upon picking up mushrooms
+            AmountPerPickup = 1, -- How many [MushroomItem] is given to the player each time they pickup
         },
         Cooldown = {
-            Minutes = 5,
+            Minutes = 5, -- Specific area cooldown in minutes
         },
-        ContextMenuInfo = {
+        ContextMenuInfo = { -- Context menu text for this zone
             Title = "Big Shroom Tings",
             Description = "If you're after the biggest bang for your buck, then you've chose the right area!",
-            Icon = "fa-solid fa-fire",
+            Icon = "fa-solid fa-fire", -- Up to you to find one that works if you change this [https://fontawesome.com/]
             IconColor = "red",
         },
     },
