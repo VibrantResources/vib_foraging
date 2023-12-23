68e990fac0439798fead95f33f89f569766a6d38
Jake Murray <j1993murray@hotmail.com>
2023-12-23 13:16:11 +0000

    Update commented text in Config

--- config.lua
+++ config.lua
@@ -2,15 +2,15 @@ Config = Config or {}
 
 Config.Debug = true
 Config.MoneyItem = "money" -- Money item taken when paying for a foraging area
--- [Config.CleanUpTimer] is designed to allow automatic removal of spawned props and peds. If this timer is HIGHER than an Area's [Cooldown.Minutes], the timer will BECOME
--- that Area's [Cooldown.Minutes] time, if it's LESS then it'll stay as whatever is defined here
+-- [Config.CleanUpTimer] is designed to allow automatic removal of spawned props and peds. If this timer is HIGHER than an Area's [Cooldown.Minutes], the timer will
+-- BECOME that Area's [Cooldown.Minutes] time, if it's LESS then it'll stay as whatever is defined here
 Config.CleanUpTimer = 0.5
 
 
 Config.DealerPed = {
     ChanceForPedAggression = 15, -- % chance for peds to be aggressive upon interaction
-    PedModel = "s_m_m_movspace_01", -- Ped model for shop
-    Location = vector4(1565.35, 3212.97, 40.41, 104.87), -- Location shop ped spawns
+    PedModel = "s_m_m_movspace_01", -- Shop ped model
+    Location = vector4(1565.35, 3212.97, 40.41, 104.87), -- Location of shop ped
     MushroomShop = {
         MoneyItem = "black_money", -- Money item given after selling items
         ItemsForSale = { -- [Item] / [item sale price]
@@ -23,14 +23,14 @@ Config.DealerPed = {
 Config.ForageLocations = {
     {
         AreaName = 'Big Shroom Tings', -- Must be unique for cooldown purposes
-        AreaCoords = vector3(1565.35, 3212.97, 40.41), -- Where mushroom area is
+        AreaCoords = vector3(1565.35, 3212.97, 40.41), -- Where you want the mushroom to spawn
         AreaRadius = 15.0, -- Radius mushrooms will spawn randomly around [AreaCoords] above (Keep this as a float to allow peds to wander)
-        AreaCost = 1500, -- How much it charges the player to be given each location
+        AreaCost = 1500, -- How much this location will charge the player
         MushroomsInfo = {
-            MushroomAmount = 5, -- How many muhsrooms spawn
-            MushroomModel = "prop_stoneshroom1", -- Model that spawns (Can be a filing cabinet if you really fucking wanted XD)
+            MushroomAmount = 5, -- How many mushrooms spawn
+            MushroomModel = "prop_stoneshroom1", -- Model that spawns (Can realisticly be whatever you want)
             MushroomItem = "schwingleborp_mushrooms", -- Name of item given to player upon picking up mushrooms
-            AmountPerPickup = 1, -- How many [MushroomItem] is given to the player each time they pickup
+            AmountPerPickup = 1, -- How many [MushroomItem] is given to the player each time they collect
         },
         NudistInfo = {
             AmountOfNudists = 5, -- How many NPC's spawn
