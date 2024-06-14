e827f0d3f42b9f3a4a8623c628398961d396e105
Jake Murray <j1993murray@hotmail.com>
2024-06-14 14:33:59 +0100

    Remove Wait() & add local variable

--- client/cleanup.lua
+++ client/cleanup.lua
@@ -3,13 +3,14 @@
 -----------------
 
 RegisterNetEvent('foraging:client:CleanUpTimer', function(data)
+    local timer = 0
+
     if Config.CleanUpTimer > data.Cooldown.Minutes then
 	    timer = data.Cooldown.Minutes * 60000
     else
         timer = Config.CleanUpTimer * 60000
     end
 
-    Wait(250)
     Wait(timer)
     RemoveBlip(forageBlip)
 end)
